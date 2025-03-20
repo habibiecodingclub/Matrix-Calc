e365ec86dc6d841e0ebb0949524a7d5da7a1fb0f
Muhammadismail30 <mhmmdismailhaji@gmail.com>
2025-03-20 16:35:26 +0700

    tambahkan fungsi perkalian dan determinan

--- Main.js
+++ Main.js
@@ -20,21 +20,55 @@ function tambahMatriks(matriks1, matriks2){
 }
 // Pengurangan Matriks
 function kurangMatriks (matriks1, matriks2){
-    // let hasil = [];
+    let hasil = [];
 
-    // for(let i = 0; i < matriks1.length; i++){
-    //     hasil[i] = [];
-    //     for(let j = 0; j < matriks1.length[i]; j++){
-    //         hasil[i][j] = Math.abs(matriks1[i][j]) - Math.abs(matriks2[i][j]);
-    //     }
-    // }
-    // console.log(hasil);
-    // Masih error
+    for(let i = 0; i < matriks1.length; i++){
+        hasil[i] = [];
+        for(let j = 0; j < matriks1[i].length; j++){
+            hasil[i][j] = matriks1[i][j] - matriks2[i][j];
+        }
+    }
+    return hasil;
 }
 
 
 // Perkalian Matriks
+function kaliMatriks(matriks1, matriks2){
+    let hasil = [];
+
+    for (let i = 0; i < matriks1.length; i++){
+        hasil[i] = [];
+        for (let j = 0; j < matriks2[0].length; j++){
+            hasil[i][j] = 0;
+            for (let k = 0; k < matriks1[0].length; k++){
+                hasil[i][j] += matriks1[i][k] * matriks2[k][j];
+            }
+        }
+    }
+    return hasil;
+}
+
 // Determinan Matriks
+function determinanMatriks(matriks){
+    let hasil = 0;
+    if (matriks.length == 2){
+        return matriks[0][0] * matriks[1][1] - matriks[0][1] * matriks[1][0];
+        } else {
+            for (let i = 0; i < matriks.length; i++){
+                let subMatriks = [];
+                for (let j = 1; j < matriks.length; j++){
+                    subMatriks[j-1] = [];
+                    for (let k = 0; k < matriks.length; k++){
+                        if (k != i){
+                            subMatriks[j-1].push(matriks[j][k]);
+                        }
+                    }
+                }
+            }
+        }
+    return hasil;
+}
+
 
 
 
@@ -48,7 +82,15 @@ let B = [
 
 tampilkanMatriks(A);
 tampilkanMatriks(B);
-console.log("-----------------")
+console.log("---------Tambah--------")
 let tambah = tambahMatriks(A, B);
 tampilkanMatriks(tambah);
-console.log("-----------------")
+console.log("---------Kurang--------")
+let kurang = kurangMatriks(A, B);
+tampilkanMatriks(kurang);
+console.log("---------Kali--------")
+let kali = kaliMatriks(A, B);
+tampilkanMatriks(kali);
+console.log("---------Det--------")
+let det = determinanMatriks(A);
+console.log(det)
