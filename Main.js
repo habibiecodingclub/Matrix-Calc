dfc39d439d3c938306b075e694a4f74bc02f848b
Ameeeek <ameeeekwork@gmail.com>
2025-03-29 16:22:04 +0800

    selesaikan semua tombol dan integrasikan penambahan

--- Main.js
+++ Main.js
@@ -38,9 +38,23 @@ function pertambahanMatriks(matrixA, matrixB) {
 
 
 document.addEventListener('DOMContentLoaded', () => {
-    const matrixAinput = document.querySelectorAll('.matriksA:nth-child(2) .input .inputan input');
-    const matrixBinput = document.querySelectorAll('.matriksA:nth-child(3) .input .inputan input');
+    const matrixAinput = document.querySelectorAll('.matriks:nth-child(2) .input .inputan input');
+    const matrixBinput = document.querySelectorAll('.matriks:nth-child(3) .input .inputan input');
     const tombolTambah = document.getElementById('tambahBtn');
+
+
+    const tombolkali = document.getElementById('kaliBtn')
+
+    const x1 = document.getElementById('hasil-x1')
+    const x2 = document.getElementById('hasil-x2')
+    const x3 = document.getElementById('hasil-x3')
+    const y1 = document.getElementById('hasil-y1')
+    const y2 = document.getElementById('hasil-y2')
+    const y3 = document.getElementById('hasil-y3')
+    const z1 = document.getElementById('hasil-z1')
+    const z2 = document.getElementById('hasil-z2')
+    const z3 = document.getElementById('hasil-z3')
+
 
 
     tombolTambah.addEventListener('click', () => {
@@ -48,8 +62,19 @@ document.addEventListener('DOMContentLoaded', () => {
         const matrixB = buatMatrix(matrixBinput);
 
         const hasilPertambahan = pertambahanMatriks(matrixA, matrixB);
-        if (hasilPertambahan) {
-        console.log('Hasil akhir di console:', hasilPertambahan);
-        }
+        // if (hasilPertambahan) {
+        // console.log('Hasil akhir di console:', hasilPertambahan);
+        // }
+        x1.value = hasilPertambahan[0][0]
+        x2.value = hasilPertambahan[0][1]
+        x3.value = hasilPertambahan[0][2]
+        y1.value = hasilPertambahan[1][0]
+        y2.value = hasilPertambahan[1][1]
+        y3.value = hasilPertambahan[1][2]
+        z1.value = hasilPertambahan[2][0]
+        z2.value = hasilPertambahan[2][1]
+        z3.value = hasilPertambahan[2][2]
     });
 });
+
+
